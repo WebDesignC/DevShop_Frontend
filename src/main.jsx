cd7c5e97025814436448156c8f07dd0a8f932494
Rivi03 <riosschrodinger@gmail.com>
2025-08-21 23:03:15 -0600

    Wraps app in router for navigation support
    Enables client-side routing by introducing a router provider.
    Prepares the app for future navigation features and route handling.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Footer from './components/Footer.jsx'
+import { BrowserRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <BrowserRouter>
     <Footer/> 
+  </BrowserRouter>
   </StrictMode>,
 )
