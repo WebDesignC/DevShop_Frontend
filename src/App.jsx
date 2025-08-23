20ac92231352af3c4e9f15bc6cb0d65abc98c244
Kaleb <rtkaleb@gmail.com>
2025-08-23 15:05:45 -0600

    Se borraron archivos repetidos y se cambiaron nombres a los que se quedaron. Se modificó el footer para cuadrar dimensiones e íconos. Se agregaron las páginas de estilos y se modificaron archivos para coincidir con rutas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import Home from './pages/Home'
+import Home from './pages/HomePage'
+import AboutUs from './pages/AboutUs'
+import Login from './pages/LoginPage'
 
 function App() {
   return (
