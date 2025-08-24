2a79046b497c925efccf2cb2d754aef8b0e0e7ae
Rivi03 <riosschrodinger@gmail.com>
2025-08-24 13:53:06 -0600

    se agregan las rutas

---
+++ src/routes/routes.jsx
@@ -0,0 +1,42 @@
+
+import Home from '../pages/HomePage';
+import AboutUs from '../pages/AboutUsPage';
+import Login from '../pages/LoginPage';
+
+const routes = [
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/about',
+    element: <AboutUs />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+
+  // {
+  //   path: '/help-center',
+  //   element: <HelpCenter />
+  // },
+  // {
+  //   path: '/faq',
+  //   element: <FAQ />
+  // },
+  // {
+  //   path: '/cart',
+  //   element: <Cart />
+  // },
+  // {
+  //   path: '/favorites',
+  //   element: <Favorites />
+  // },
+  // {
+  //   path: '/privacy',
+  //   element: <PrivacyPolicy />
+  // }
+];
+
+export default routes;
