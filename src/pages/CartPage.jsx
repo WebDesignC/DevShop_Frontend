64c39239cdd66e28712e0ddcf794a4da4b89ee44
Denisse <yuleiditho@gmail.com>
2025-08-29 00:07:37 -0600

    refactor: reorganización de componentes y sus rutas de importación

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -133,5 +133,3 @@ export const CartPage = () => {
     </div>
   );
 };
-
-export default CartPage
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,29 +1,30 @@
-import React from "react";
-import "../styles/NotFound.css";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import '../styles/NotFound.css';
 
-const NotFoundPage = () => {
+export const NotFoundPage = () =>  {
     return (
-        <div className="notfound-container">
-            <div className="not-found-img-container">
-                <img
-                className="not-found-img"
-                src="https://i.postimg.cc/HWJkx4WY/Copilot-20250825-224458.png"
-                alt="Not Found"
-                style={{maxWidth:'400px'}}
-                />
+        <div className="not-found-container">
+            <div className="not-found-content">
+                <h1 className="not-found-error-code">404</h1>
+                <h2 className="not-found-title">¡Parece que te has desviado del camino!</h2>
+                <p className="not-found-description">
+                    La página que buscas no está disponible o nunca existió.
+                </p>
+                <div className="not-found-actions">
+                    <Link
+                        to="/"
+                        className="home-button-404"
+                    >
+                        Volver a la página principal
+                    </Link>
+                    <Link
+                        to="/productos"
+                        className="home-button-404 secondary"
+                    >
+                        Explorar productos
+                    </Link>
+                </div>
             </div>
-            <h1 className="notfound-code">404</h1>
-            <h2 className="notfound-title">Página no encontrada</h2>
-            <p className="notfound-text">
-                Lo sentimos, la página que buscas no existe o fue movida.
-            </p>
-            <Link to="/" className="notfound-btn">
-                ⬅️ Volver al inicio
-            </Link>
         </div>
-
     );
 };
-
-export default NotFoundPage;
